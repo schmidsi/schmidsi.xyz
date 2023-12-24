5b381e899f38685d97a1ced8ea8ba811fb4c6f2a
Simon Emanuel Schmid <simon@schmid.io>
2023-12-24 18:53:42 +0100

    Fill page head

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,7 +1,15 @@
 import type { NextraThemeLayoutProps } from "nextra";
+import Head from "next/head";
 
 export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
   console.log(pageOpts);
 
-  return <div>{children}</div>;
+  return (
+    <>
+      <Head>
+        <title>{pageOpts.title} | Simon Emanuel Schmid</title>
+      </Head>
+      <div>{children}</div>
+    </>
+  );
 }
