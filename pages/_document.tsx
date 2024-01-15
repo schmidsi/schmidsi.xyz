a761ca0d6dc0b120a3abaa999d6ac34683674caf
Simon Emanuel Schmid <simon@schmid.io>
2024-01-15 12:42:49 +0000

    More links on homepage

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png',
-  }
+    title: "Simon Emanuel Schmid`s Homepage",
+    description: "Clone and deploy your own Next.js portfolio in minutes.",
+    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+  };
 
   return (
     <Html lang="en">
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@schmid_si" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
@@ -27,5 +27,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
