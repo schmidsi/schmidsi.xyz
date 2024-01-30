1d183b580b21ee32ed4d12e0d845b3ebd7a65f67
Simon Emanuel Schmid <simon@schmid.io>
2024-01-30 13:12:03 +0000

    Add logo to content to allow back linking

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,6 +1,7 @@
 import type { NextraThemeLayoutProps } from 'nextra';
 import Head from 'next/head';
 import { Figtree } from 'next/font/google';
+import Link from 'next/link';
 
 const figtree = Figtree({
   subsets: ['latin'],
@@ -11,14 +12,19 @@ export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
   console.log(pageOpts);
 
   return (
-    <>
+    <div className={`${figtree.variable} font-sans my-8 mx-4`}>
       <Head>
         <title>{pageOpts.title} | Simon Emanuel Schmid</title>
       </Head>
 
-      <main className={`${figtree.variable} font-sans mt-8 mx-4 prose`}>
-        {children}
-      </main>
-    </>
+      <Link href="/">
+        <header className="text-xl font-bold text-gray-400 leading-negative">
+          <span className="text-gray-900">S</span>imon <br />{' '}
+          <span className="text-gray-900">E</span>manuel <br />{' '}
+          <span className="text-gray-900">S</span>chmid
+        </header>
+      </Link>
+      <main className="prose mt-8 mb-8">{children}</main>
+    </div>
   );
 }
