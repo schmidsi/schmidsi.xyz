be2ec3e8bc0fe6993c5a544aafaa314b053aaa2f
Simon Emanuel Schmid <simon@schmid.io>
2025-10-31 14:45:04 +0100

    Fix Tailwind content paths for App Router

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,md,mdx}',
     './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './content/**/*.{js,ts,jsx,tsx,md,mdx}',
   ],
   theme: {
     extend: {
